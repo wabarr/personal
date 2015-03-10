8abdde1d2a9efb4e8f0319e8862cd0719f52b12a
wabarr <wabarr@gmail.com>
2015-03-10 13:58:00 -0400

    update CV

--- assets/CV/makeCV.sh
+++ assets/CV/makeCV.sh
@@ -1,5 +1,7 @@
 #before running, copy the html code from this url http://wabarr.github.io/real-time-CV-extract-for-PDF-conversion/
 #save the html to the file /Users/wabarr/Documents/wabarr-personal-website/assets/CV/extract_of_onlineCV.html
+#the easiest is to use chrome and right click to inspect element.  Then cut and paste the whole thing, being sure to look at the bottom
+#to make sure feedly or another browser extension hasn't added in garbage
 #then run the two lines below
 
 basedir=/Users/wabarr/Documents/wabarr-personal-website/assets/CV/
